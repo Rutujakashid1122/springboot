0f07b92ab44d3822f2b68725b6bca301e584847a
Rutujakashid1122 <kashidrutuja2@gmail.com>
2025-07-03 11:12:37 +0530

    Add files via upload

---
+++ SpringbootdataappApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springbootdataapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdataappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdataappApplication.class, args);
+	}
+
+}
